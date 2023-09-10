8f5d2c3905f8f7ac330f4a07c79ee0b0fce8bb20
MarcosEduardoA <marcoseduardogo@gmail.com>
2023-09-10 09:27:11 -0300

    Starting to implement Board and Piece

--- chess_system/application/Progam.java
+++ chess_system/application/Progam.java
@@ -1,11 +1,11 @@
 package chess_system.application;
 
-import chess_system.application.board_game.Position;
+import chess_system.application.board_game.Board;
 
 public class Progam {
     public static void main(String[] args) {
-        Position pos = new Position(3, 5);
 
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+
     }
 }
---
+++ chess_system/application/board_game/Piece.java
@@ -0,0 +1,16 @@
+package chess_system.application.board_game;
+
+public class Piece {
+    
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
